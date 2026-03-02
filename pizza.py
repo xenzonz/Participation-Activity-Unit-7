8bc03818f4b34f93d064e4824c38144304e24112
sam cocquyt <samcocquyt04@gmail.com>
2026-03-01 23:13:12 -0500

    imrpoved main logic

--- pizza.py
+++ pizza.py
@@ -1,15 +1,18 @@
 print("hello world")
 
+#flag
 ordering = True
 
 while ordering:
     topping = input("Enter a pizza topping (type 'quit' to end): ")
 
+    #blank input guard clause
     if topping == "":
         print("Please enter a valid topping.")
+        continue #reset loop
 
-    if topping == "quit":
-        ordering = False
+    if topping.lower().strip() == "quit":
+        ordering = False #set flag false to end loop
     else:
         print(f"I'll add {topping} to your pizza.")
 
