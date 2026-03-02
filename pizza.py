259caeb4c1004c04eed6b83ae96ff231bf590f9c
sam cocquyt <samcocquyt04@gmail.com>
2026-03-01 23:15:35 -0500

    final touches

--- pizza.py
+++ pizza.py
@@ -1,18 +1,19 @@
-print("hello world")
+#print("hello world")
 
 #flag
 ordering = True
 
 while ordering:
-    topping = input("Enter a pizza topping (type 'quit' to end): ")
+    topping = input("Enter a pizza topping (type 'quit' to end): ").strip()
 
     #blank input guard clause
     if topping == "":
         print("Please enter a valid topping.")
         continue #reset loop
 
-    if topping.lower().strip() == "quit":
+    if topping.lower() == "quit":
         ordering = False #set flag false to end loop
+        print("Program ended.")
     else:
         print(f"I'll add {topping} to your pizza.")
 
